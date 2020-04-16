ef08545120fbaa2e94816f55a850cd2cde0e3d13
Borges <gustavoborges98@hotmail.com>
2020-04-16 11:06:57 -0300

    Atualização do WorkSpace e novo nome
    Atualização do WorkSpace e novo nome

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Chatbot - OpenWorkSpace
  Para o tempo de quarentena e home office, criei uma aplicação em Python para abrir programas e janelas conforme o dia da semana e hora do dia.
+
+A utilização é bem simples..
+
+O software está divido em 3 períodos distintos, onde você pode gerenciar as aplicações a serem abertas em cada um deles.
+
+Dias da Samana antes de 12:55hrs e depois das 19hrs;
+Dias de semana entre 12:55hrs e 19hrs;
+Finais de semana;
+
+Está programado para as aplicações que estou utilizando na minha rotina, mas pode ser adaptado para a sua, apenas mududando os caminhos no código, das aplicações a serem abertas para você em cada horário.
--- SmartWorkSpace.py
+++ SmartWorkSpace.py
@@ -9,14 +9,19 @@ print (horaAtual)
 
 if horaAtual.tm_wday < 5:
     if 13 <= horaAtual.tm_hour < 19 or 12 < horaAtual.tm_hour < 19 and horaAtual.tm_min > 50:
-        os.startfile('notepad.exe')
+
+        os.startfile('http://trello.com')
+        os.startfile('http://mwm.ibridge.net.br/login')
+        os.tartfile('notepad.exe')
         os.startfile(r"C:\Users\gusta\AppData\Local\slack\slack.exe")
         os.startfile(r"C:\Users\gusta\Desktop\IBRIDGE")
+        os.startfile(r"C:\Program Files\Microsoft Power BI Desktop\bin\PBIDesktop.exe")
 
     else:
         os.startfile('https://youtube.com')
         os.startfile(r"C:\Program Files\JetBrains\PyCharm Community Edition 2019.2.5\bin\pycharm64.exe")
+        os.startfile(r"C:\Users\gusta\eclipse\java-2019-09\eclipse\eclipse.exe")
 else:
     os.startfile('http://netflix.com')
     os.startfile(r"C:\Users\gusta\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Yooiistudios\Yooii Apps\Mobile Buddy.exe")
-
+    os.startfile(r"C:\Users\gusta\AppData\Local\Programs\Microsoft VS Code\Code.exe")
